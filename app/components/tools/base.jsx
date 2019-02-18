cc00d62a5f47c1fa913fbac64cd6f339de3875b8
zhangxuekang <zhangxuekang@gmail.com>
2019-02-18 14:02:51 +0800

    opt: tool title

--- app/components/tools/base.jsx
+++ app/components/tools/base.jsx
@@ -1,25 +1,54 @@
 import React from 'react'
 import './tools.less'
-import { Tooltip, OverlayTrigger } from 'react-bootstrap'
+import { Tooltip, Overlay } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
 export default function(Target) {
   class Base extends React.Component {
+    constructor(props) {
+      super(props)
+      this.attachRef = target => this.setState({ target })
+      this.state = { show: false }
+      this.showTitle = this.showTitle.bind(this)
+      this.closeTitle = this.closeTitle.bind(this)
+    }
+
+    showTitle() {
+      this.setState({
+        show: true
+      })
+      window.addEventListener('click', this.closeTitle)
+    }
+
+    closeTitle() {
+      this.setState({
+        show: false
+      })
+      window.removeEventListener('click', this.closeTitle)
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('click', this.closeTitle)
+    }
+
     render() {
+      const { show, target } = this.state
       return (
-        <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
+        <React.Fragment>
+          <div
+            className="tool"
+            ref={this.attachRef}
+            onMouseOver={this.showTitle}
+            onMouseLeave={this.closeTitle}
+          >
+            <Target {...this.props} />
+          </div>
+          <Overlay target={target} show={show} placement="bottom">
             <Tooltip className='tool-tooltip' id={`tool-tooltip-${this.props.type}`}>
               {this.props.title}
             </Tooltip>
-          }
-        >
-          <div className="tool">
-            <Target {...this.props} />
-          </div>
-        </OverlayTrigger>
+          </Overlay>
+        </React.Fragment>
       )
     }
   }
